b14de3562aa51086e78d46fe84e9a2527d341472
Masahiro TANAKA <masa16.tanaka@gmail.com>
2016-05-23 19:59:47 +0900

    fix bessel demo

--- gsl/bessel/mkmd.rb
+++ gsl/bessel/mkmd.rb
@@ -3,12 +3,14 @@ repo = "numo-gnuplot-demo"
 dir  = `git rev-parse --show-prefix`.chomp
 
 open("README.md","w") do |f|
+  f.puts "## Ruby script\n\n"
   Dir.glob("plot*.rb") do |src|
-    f.puts "## #{dir}\n\n```ruby"
+    f.puts "* #{dir}#{src}\n\n```ruby"
     f.puts IO.read(src)
     f.puts "```\n\n"
   end
 
+  f.puts "## Result plot\n\n"
   Dir.glob("*.png") do |img|
     link = "https://raw.github.com/#{user}/#{repo}/master/#{dir}#{img}"
     f.puts "![#{img}](#{link})"
